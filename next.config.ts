f95572a16460f3fec61c29472465e15341b8eae7
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-09-02 14:08:22 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false,
+typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+swcMinify: false,
+images: { unoptimized: true },
+output: 'standalone',
 };
 
 export default nextConfig;
